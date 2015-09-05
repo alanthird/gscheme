8803f483ab78266284f337d8afb7e6a418647ea8
Alan Third <alan@idiocy.org>
2015-09-05 19:32:16 +0100

    Add basic REPL

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"github.com/alanthird/gscheme/evaluator"
+	"github.com/alanthird/gscheme/parser"
+	"github.com/bobappleyard/readline"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	env := evaluator.BuildEnvironment()
+
+	for {
+		l, err := readline.String("> ")
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "%s\n", err)
+			break
+		}
+
+		f, err := parser.Parse(strings.NewReader(l))
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "%s\n", err)
+			readline.AddHistory(l)
+		}
+
+		r, err := evaluator.Eval(env, f)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "%s\n", err)
+		} else {
+			fmt.Println(r)
+		}
+		readline.AddHistory(l)
+	}
+}
