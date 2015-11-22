56beb6bf86ed2338f468672b5d48a2483f05abe5
Alan Third <alan@idiocy.org>
2015-11-22 11:08:41 +0000

    Fix printing of errors

--- main.go
+++ main.go
@@ -19,19 +19,19 @@ func main() {
 			break
 		}
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "%s\n", err)
+			fmt.Fprintln(os.Stderr, err)
 			break
 		}
 
 		f, err := parser.Parse(strings.NewReader(l))
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "%s", err)
+			fmt.Fprintln(os.Stderr, err)
 			readline.AddHistory(l)
 		}
 
 		r, err := evaluator.Eval(env, f)
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "%s", err)
+			fmt.Fprintln(os.Stderr, err)
 		} else {
 			fmt.Println(r)
 		}
